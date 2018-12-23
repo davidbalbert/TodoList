9d32310318711292d675bfb99ad91ec0c6624756
David Albert <davidbalbert@gmail.com>
2018-12-23 11:35:03 -0500

    Initial Commit

---
+++ TodoList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TodoList
+//
+//  Created by David Albert on 12/23/18.
+//  Copyright © 2018 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TodoList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by David Albert on 12/23/18.
+//  Copyright © 2018 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
